8a0cfdbdfd657f93e5710be3d9ab2b14560e4d3c
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-08-01 23:36:15 +0700

    ci/cd pipeline

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.static("public"));
 
-//view engine
+//View engine
 app.set("views", "./views");
 app.set("view engine", "pug");
 
